78d0639d550c66e482fc1c4af65374f9feb6b80d
arlanviray <arlan.viray@gmail.com>
2022-06-11 02:03:21 +0100

    Trader page updates - added input field to Strike dropdown plus logic, set z-index to element with dropdown to avoid transparent boxes, made Amount value input field, removed top panel on TradeView widget

--- dev/src/js/components/component.dropdowSelect.js
+++ dev/src/js/components/component.dropdowSelect.js
@@ -5,10 +5,36 @@ export default {
 
     init() {
         this.globals.elem.forEach((dropdownSelect) => {
-            this.btnValues(dropdownSelect)
-
-            // buttons
             const btn = dropdownSelect.querySelector(".ds-btn")
+            const listItems = dropdownSelect.querySelectorAll("li")
+            const input = dropdownSelect.querySelector("input[name='info-amount']")
+            let inputFocus = false
+
+            // set value
+            if (input) {
+                this.valueFromInputField(dropdownSelect, input.value)
+
+                // input events
+                input.addEventListener("click", (e) => {
+                    inputFocus = true
+                    listItems.forEach((item) => item.classList.remove("info-active"))
+                    this.valueFromInputField(dropdownSelect, e.target.value)
+                }, false)
+
+                input.addEventListener("blur", () => {
+                    inputFocus = false
+                    btn.parentElement.classList.remove("active")
+                }, false)
+
+                input.addEventListener("keyup", (e) => {
+                    this.valueFromInputField(dropdownSelect, e.target.value)
+                }, false)
+
+            } else {
+                this.valueFromListItem(dropdownSelect)
+            }
+
+            // button events
             btn.addEventListener("click", () => {
                 if (btn.parentElement.classList.contains("active")) {
                     btn.parentElement.classList.remove("active")
@@ -19,35 +45,41 @@ export default {
 
             btn.addEventListener("blur", () => { // added tabindex="0" to work in div element
                 setTimeout(() => {
-                    btn.parentElement.classList.remove("active")
+                    if (!inputFocus) {
+                        btn.parentElement.classList.remove("active")
+                    }
                 }, 100)
             }, false)
 
             // infos
-            const listItems = dropdownSelect.querySelectorAll("li")
             listItems.forEach((item) => {
                 item.addEventListener("click", () => {
-                    console.log(item)
                     // reset
                     listItems.forEach((item) => item.classList.remove("info-active"))
                     // set state
                     item.classList.add("info-active")
                     // set value
-                    this.btnValues(dropdownSelect)
+                    this.valueFromListItem(dropdownSelect)
                 }, false)
             })
         })
     },
 
-    btnValues(dropdownSelect) {
+    valueFromListItem(dropdownSelect) {
         const listItems = dropdownSelect.querySelectorAll("li")
         listItems.forEach((item) => {
             if (item.classList.contains("info-active")) {
-                const [value1, value2] = item.textContent.split("|")
+                const [itemVal1, itemVal2] = item.textContent.split("|")
                 const btn = dropdownSelect.querySelector(".ds-btn")
-                btn.querySelector(".ds-value1").textContent = value1
-                btn.querySelector(".ds-value2").textContent = value2
+                btn.querySelector(".ds-value1").textContent = itemVal1
+                btn.querySelector(".ds-value2").textContent = itemVal2
             }
         })
+    },
+
+    valueFromInputField(dropdownSelect, inputVal) {
+        const btn = dropdownSelect.querySelector(".ds-btn")
+        btn.querySelector(".ds-value1").textContent = inputVal
+        btn.querySelector(".ds-value2").textContent = "0%" // calculate percentage value
     },
 }
