2192a365cd05eefbd7d1b26afab6de3fdd23598c
arlanviray <arlan.viray@gmail.com>
2022-06-19 14:49:00 +0100

    built pages only

---
+++ assets/page.trader.2a5cac41.js
@@ -0,0 +1,1 @@
+import e from"./component.tradingviewWidget.5e0ba4fc.js";import"./main.825986c3.js";var s={globals:{elem:document.querySelector(".trader")},init(){new MutationObserver(t=>{console.log("sidenav has changed!"),e.createGraph()}).observe(this.globals.elem.querySelector(".sidenav"),{childList:!0,characterData:!0})}};export{s as default};
